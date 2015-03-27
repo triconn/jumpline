e9474fa16f678ee52c3f6fd75c4808cb7e968f5f
Aaron Tribou <aaron.tribou@gmail.com>
2015-03-26 20:10:34 -0500

    Add node engine to package.json Adjust test

--- test/controllers/default.test.js
+++ test/controllers/default.test.js
@@ -3,17 +3,15 @@ var expect = require('code').expect();
 
 describe('Homepage', function () {
 
-  describe('#index()', function () {
-    it('should return iQueue', function (done) {
-      request(sails.hooks.http.app)
-        .post('/')
-        .expect(200)
-        .end(function (err, res) {
-          if (err) return done(err);
-          expect(res.body).to.contain('iQueue');
-          done();
-        });
-    });
+  it('should return iQueue', function (done) {
+    request(sails.hooks.http.app)
+      .post('/')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).to.contain('iQueue');
+        done();
+      });
   });
 
 });
