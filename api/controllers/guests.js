79dc880f0760866265dd1c2acfd7d5dea957f2b8
Aaron Tribou <aaron.tribou@gmail.com>
2015-08-05 22:16:06 -0500

    Add Joi validation on get and create guests routes Includes create guest request payload Includes get and create guests responses

--- api/controllers/guests.js
+++ api/controllers/guests.js
@@ -1,4 +1,5 @@
 var Boom = require('boom');
+var Joi = require('joi');
 
 module.exports = {
 
@@ -16,6 +17,20 @@ module.exports = {
         reply(result);
 
       });
+    },
+    response: {
+      schema: Joi.object().keys({
+        guests: Joi.array().items(Joi.object().keys({
+          id: Joi.number(),
+          name: Joi.string().max(248).required(),
+          phone: Joi.number().min(1111111111).max(9999999999).precision(0),
+          estimate: Joi.number(),
+          status: Joi.any().allow('new', 'notified', 'called', 'completed', 'cancelled'),
+          createdAt: Joi.date().iso(),
+          updatedAt: Joi.date().iso(),
+          estimatedAt: Joi.date().iso()
+        }))
+      })
     }
   },
 
@@ -33,6 +48,29 @@ module.exports = {
         .code(201);
 
       });
+    },
+    validate: {
+      payload: Joi.object().keys({
+        guest: {
+          name: Joi.string().max(248).required(),
+          phone: Joi.number().min(1111111111).max(9999999999).precision(0),
+          estimate: Joi.number()
+        }
+      })
+    },
+    response: {
+      schema: Joi.object().keys({
+        guest: {
+          id: Joi.number(),
+          name: Joi.string().max(248).required(),
+          phone: Joi.number().min(1111111111).max(9999999999).precision(0),
+          estimate: Joi.number(),
+          status: Joi.any().allow('new', 'notified', 'called', 'completed', 'cancelled'),
+          createdAt: Joi.date().iso(),
+          updatedAt: Joi.date().iso(),
+          estimatedAt: Joi.date().iso()
+        }
+      })
     }
   }
 
