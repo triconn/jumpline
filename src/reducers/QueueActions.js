8cfb78c81e99dc6cabe1c5ed0f4cd72b38d8ee07
Aaron Tribou <aaron.tribou@gmail.com>
2016-01-30 23:04:48 -0600

    Add addGuest redux async actions Add getGuests redux async actions Add iQueue class helper with get, add, notify, and complete Add npm debug package for logging

--- src/reducers/QueueActions.js
+++ src/reducers/QueueActions.js
@@ -1,29 +1,123 @@
 import { Actions } from '../lib/constants.js';
+import iQueue from '../helpers/iqueue.js';
+
+
+export function addGuestRequest() {
+  return {
+    type: Actions.ADD_GUEST_REQUEST,
+  };
+}
+
+export function addGuestSuccess() {
+  return {
+    type: Actions.ADD_GUEST_SUCCESS,
+  };
+}
+
+export function addGuestFailure(error) {
+  return {
+    type: Actions.ADD_GUEST_FAILURE,
+    error,
+  };
+}
 
 export function addGuest(guest) {
+
+  return dispatch => {
+
+    dispatch(addGuestRequest());
+
+    return new iQueue().addGuest(guest)
+    .then(body => {
+
+      dispatch(addGuestSuccess());
+
+    })
+    .catch(error => {
+
+      dispatch(addGuestFailure(error));
+
+    });
+  }
+}
+
+
+export function getGuestsRequest() {
   return {
-    type: Actions.ADD_GUEST,
-    guest,
+    type: Actions.GET_GUESTS_REQUEST,
   };
 }
 
-export function getCurrentGuests(guest) {
+export function getGuestsSuccess(guests) {
   return {
-    type: Actions.GET_GUESTS,
-    guest,
+    type: Actions.GET_GUESTS_SUCCESS,
+    guests,
   };
 }
 
-export function notifyGuest(id) {
+export function getGuestsFailure(error) {
   return {
-    type: Actions.NOTIFY_GUEST,
-    guest,
+    type: Actions.GET_GUESTS_FAILURE,
+    error,
   };
 }
 
-export function completeGuest(id) {
+export function getGuests() {
+
+  return dispatch => {
+
+    dispatch(getGuestsRequest());
+
+    return new iQueue().getGuests()
+    .then(body => {
+
+      dispatch(getGuestsSuccess(body));
+
+    })
+    .catch(error => {
+
+      dispatch(getGuestsFailure(error));
+
+    });
+  }
+}
+
+
+export function notifyGuestRequest() {
   return {
-    type: Actions.COMPLETE_GUEST,
-    guest,
+    type: Actions.NOTIFY_GUEST_REQUEST,
+  };
+}
+
+export function notifyGuestSuccess() {
+  return {
+    type: Actions.NOTIFY_GUEST_SUCCESS,
+  };
+}
+
+export function notifyGuestFailure(error) {
+  return {
+    type: Actions.NOTIFY_GUEST_FAILURE,
+    error,
+  };
+}
+
+
+export function completeGuestRequest() {
+  return {
+    type: Actions.COMPLETE_GUEST_REQUEST,
+  };
+}
+
+export function completeGuestSuccess() {
+  return {
+    type: Actions.COMPLETE_GUEST_SUCCESS,
+  };
+}
+
+export function completeGuestFailure(error) {
+  return {
+    type: Actions.COMPLETE_GUEST_FAILURE,
+    error,
   };
 }
