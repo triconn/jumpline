c02eacbdcb92a5172aab6972869f268faca5b980
Aaron Tribou <aaron.tribou@gmail.com>
2016-02-09 22:49:22 -0600

    Switch dev workflow to hapi-webpack-plugin + BrowserSync Have hapi-webpack-plugin and hapi-swagger only load in development Update outdated dependencies Clean-up webpack dev config for non-hot-reloading

--- src/config/webpack.js
+++ src/config/webpack.js
@@ -2,47 +2,44 @@ require('babel-register');
 // Webpack config file
 const Path = require('path');
 const Webpack = require('webpack');
-//const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const getJsBundle = require('../lib/utils.js').getJsBundle();
 //const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    entry: [
-      'eventsource-polyfill',
-      'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-      './src/index.browser.js',
-    ],
-    plugins: [
-      new Webpack.optimize.OccurenceOrderPlugin(),
-      new Webpack.HotModuleReplacementPlugin(),
-      new Webpack.NoErrorsPlugin(),
-      new Webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('development'),
-        },
-      }),
-      // new BrowserSyncPlugin({
-      //   proxy: 'localhost:8000',
-      //   ghostMode: false,
-      // }),
-      // new NpmInstallPlugin(),
-      // new Webpack.optimize.UglifyJsPlugin({
-      //   compress: {
-      //     warnings: false,
-      //   },
-      // }),
-    ],
+  devtool: 'cheap-module-eval-source-map',
+  //devtool: 'eval',
+  entry: [
+    './src/index.browser.js',
+  ],
   output: {
     filename: getJsBundle,
     path: Path.resolve(__dirname, '../../static/js'),
     publicPath: '/static/js/',
   },
+  plugins: [
+    //new Webpack.optimize.OccurenceOrderPlugin(),
+    new Webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development'),
+      },
+    }),
+    new BrowserSyncPlugin({
+      proxy: 'localhost:8000',
+      ghostMode: false,
+    }),
+    // new NpmInstallPlugin(),
+    // new Webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //   },
+    // }),
+  ],
 	module: {
     loaders: [
       {
         test: /\.js$|\.jsx$/,
-        exclude: /node_modules|\.swp$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           cacheDirectory: true,
@@ -78,6 +75,11 @@ module.exports = {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
       },
+    ],
+  },
+	resolve: {
+    root: [
+      Path.resolve('../../src'),
     ],
   },
   sassConfig: {
