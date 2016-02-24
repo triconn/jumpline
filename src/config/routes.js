7c9aef6d93bcfe9cb5ced7d174353294284458f7
Aaron Tribou <aaron.tribou@gmail.com>
2016-02-23 21:20:39 -0600

    Remove API routes Remove twilio helper Add initial .flowconfig

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,29 +1,6 @@
-// All the API and static routes
-const GuestController = require('../controllers/guests.js');
+// All the routes
 
 export const routes = [
-  {
-    method: 'GET',
-    path: '/guests',
-    config: GuestController.index,
-  },
-  {
-    method: 'POST',
-    path: '/guests',
-    config: GuestController.create,
-  },
-  {
-    method: 'PATCH',
-    path: '/guests/{id}/notify',
-    config: GuestController.notify,
-  },
-  {
-    method: 'PATCH',
-    path: '/guests/{id}/complete',
-    config: GuestController.complete,
-  },
-
-    // Non-controller paths
   {
     method: 'GET',
     path: '/',
