6d2ac819005e75d1a4e607f9ee686cf44ef953d0
Aaron Tribou <aaron.tribou@gmail.com>
2016-03-05 10:54:40 -0600

    Add Loader.io verification

--- src/config/routes.js
+++ src/config/routes.js
@@ -17,4 +17,18 @@ export const routes = [
       },
     },
   },
+  {
+    method: 'GET',
+    path: `/${process.env.LOADERIO_TOKEN}/`,
+    handler: (request, reply) => {
+      return reply(process.env.LOADERIO_TOKEN);
+    },
+  },
+  {
+    method: 'GET',
+    path: `/${process.env.LOADERIO_TOKEN}`,
+    handler: (request, reply) => {
+      return reply(process.env.LOADERIO_TOKEN);
+    },
+  },
 ];
