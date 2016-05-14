a693956068217968811471a96b2ac8a0cb5fee46
Aaron Tribou <aaron.tribou@gmail.com>
2016-05-14 07:22:21 -0500

    [deps] update hapi-react-views, react, react-dom, redux-thunk, inert

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -1,32 +1,38 @@
-import Debug from 'debug';
-import { applyMiddleware, createStore, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { browserHistory } from 'react-router';
-import { routerMiddleware } from 'react-router-redux';
-import rootReducer from '../reducers/index.js';
+import Debug from 'debug'
+import { browserHistory } from 'react-router'
+import { routerMiddleware } from 'react-router-redux'
+import { applyMiddleware, createStore, compose } from 'redux'
+import Thunk from 'redux-thunk'
+import rootReducer from '../reducers/index.js'
 
-const log = Debug('iq');
+const log = Debug('jl')
 
-export default function configureStore(initialState) {
+export default function configureStore (initialState) {
 
   const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(
       routerMiddleware(browserHistory),
-      thunk
+      Thunk
     ),
-    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
+    typeof window === 'object'
+      && typeof window.devToolsExtension !== 'undefined'
       ? window.devToolsExtension()
-      : f => f
-  ));
+      : (f) => f
+  ))
 
   // Enable Webpack hot module replacement for reducers
-	if (module.hot) {
-    log('Module is hot!');
+  if (module.hot) {
+
+    log('Module is hot!')
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers/index.js').default;
-      store.replaceReducer(nextRootReducer);
-    });
+
+      const nextRootReducer = rootReducer
+      store.replaceReducer(nextRootReducer)
+
+    })
+
   }
 
-  return store;
+  return store
+
 }
