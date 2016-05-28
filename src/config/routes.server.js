64cba7ecf49631bdbd27084ee08199468c1a0f99
Aaron Tribou <aaron.tribou@gmail.com>
2016-05-28 16:40:25 -0500

    Add Slack webhook for incoming sms

--- src/config/routes.server.js
+++ src/config/routes.server.js
@@ -1,5 +1,8 @@
 // All the routes
 import Server from '../index.server.js'
+import Request from 'superagent'
+import Boom from 'boom'
+import Promise from 'bluebird'
 
 const defaultRenderOptions = {
   runtimeOptions: {
@@ -36,7 +39,63 @@ const htmlHandler = (request, reply) => {
 
 }
 
+const postSlack = (from, body, url) => {
+
+  return new Promise((resolve, reject) => {
+
+    const text = `From: ${from}\n`
+               + `Message: ${body}`
+
+    Request.post(url)
+    .send({ text })
+    .end((error, response) => {
+
+      if (error) reject(error)
+      return resolve(response)
+
+    })
+
+  })
+
+}
+
+const smsReceiveHandler = (request, reply) => {
+
+  Server.log(['info', 'sms'], {
+    from: request.payload.From,
+    message: request.payload.Body,
+  })
+
+  if (!process.env.SLACK_WEBHOOK) {
+
+    return reply(Boom.serverTimeout('SLACK_WEBHOOK not set'))
+
+  }
+
+  return postSlack(
+    request.payload.From,
+    request.payload.Body,
+    process.env.SLACK_WEBHOOK
+  )
+  .then((response) => {
+
+    return reply(201)
+
+  })
+  .catch((error) => {
+
+    return reply(Boom.serverTimeout(error))
+
+  })
+
+}
+
 const routes = [
+  {
+    method: 'POST',
+    path: '/sms/receive',
+    handler: smsReceiveHandler,
+  },
   {
     method: 'GET',
     path: '/',
